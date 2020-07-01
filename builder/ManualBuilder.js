b210727135650105bbac2b21a4e94d22eadf5f5f
mykola <webmintua@gmail.com>
2020-07-01 10:21:09 +0300

    added interface and private field for builder

--- builder/ManualBuilder.js
+++ builder/ManualBuilder.js
@@ -1,20 +1,43 @@
-const Builder = require('./Builder');
+const iBuilder = require('./iBuilder');
 const Manual = require('./Manual');
 
+// Define private field
+const _manual = Symbol();
+
 // Define specialized builder for car manual
-// For simplicity it extends Builder fully, but it can have own methods which suitable for it
-class ManualBuilder extends Builder{
-  constructor(item) {
-    super(item);
+class ManualBuilder extends iBuilder{
+  constructor() {
+    super();
+    this[_manual] = null;
+  }
+  
+  setSeats(seats) {
+    this[_manual].seats = seats;
+    return this;
+  }
+  
+  setEngine(engine) {
+    this[_manual].engine = engine;
+    return this;
+  }
+  
+  setTripComputer(tc) {
+    this[_manual].tripComputer = tc;
+    return this;
+  }
+  
+  setGPS(gps) {
+    this[_manual].GPS = gps;
+    return this;
   }
   
   reset() {
-    this.item = new Manual();
+    this[_manual] = new Manual();
     return this;
   }
   
   getResult() {
-    const product = this.item;
+    const product = this[_manual];
     this.reset();
     return `This is Manual for car with ${JSON.stringify(product)} `;
   }
